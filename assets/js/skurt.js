d7bbc68ebe7e44a16227aa2405d3b254c14c00e0
Mario Zamora <maza1387@gmail.com>
2017-05-08 15:34:55 -0700

    Remove jQuery Submit

--- assets/js/skurt.js
+++ assets/js/skurt.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
 
-  // function functionName() {
-  //   $.ajax({
-  //     method: "POST",
-  //     url: "https://www.skurt.com/api/sendLink",
-  //     data: { linkId: "default", phone: "18184862507" }
-  //   })
-  //     .done(function(response) {
-  //       console.log(response);
-  //     });
-  // }
-
   var phone;
 
-//         $('#submit').on('click', function() {
+        // $('#submit').on('click', function() {
 
           phone = $('#phoneNumber').val().trim();
-          // alert(typeof phone);
 
             $.ajax({
               method: "POST",
@@ -27,6 +15,6 @@ $(document).ready(function() {
                 console.log(response);
               });
 
-//         });
+        // });
 
 });
