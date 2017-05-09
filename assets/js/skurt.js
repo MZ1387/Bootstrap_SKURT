a6c417859d06118a62a39af5a437e3464afa27b9
Mario Zamora <maza1387@gmail.com>
2017-05-08 18:41:51 -0700

    Clean JS spacing

--- assets/js/skurt.js
+++ assets/js/skurt.js
@@ -1,28 +1,33 @@
 $(document).ready(function() {
 
-  var phone;
+    var phone;
 
-        $('#submit').on('click', function(event) {
-            event.preventDefault();
-            phone = $('#phoneNumber').val();
-            if(phone != "") {
-              console.log(phone);
-              $.ajax({
+    $('#submit').on('click', function(event) {
+        event.preventDefault();
+        phone = $('#phoneNumber').val();
+        if (phone != "") {
+            console.log(phone);
+            $.ajax({
                 method: "POST",
                 url: "https://www.skurt.com/api/sendLink",
-                "headers": {"content-type": "application/x-www-form-urlencoded"},
-                data: { "linkId": "default", "phone": phone }
-              }).success(function(response) {
-                  console.log(response);
-                  $('#phoneNumber').val('');
-                }).fail(function(error) {
-                  console.log("FAILURE");
-                  console.log(error);
-                });
-              } else {
-                alert("enter proper phone number");
-              }
-        });
+                "headers": {
+                    "content-type": "application/x-www-form-urlencoded"
+                },
+                data: {
+                    "linkId": "default",
+                    "phone": phone
+                }
+            }).success(function(response) {
+                console.log(response);
+                $('#phoneNumber').val('');
+            }).fail(function(error) {
+                console.log("FAILURE");
+                console.log(error);
+            });
+        } else {
+            alert("enter proper phone number");
+        }
+    });
 
 
 
