7bb2902a9700c05144320c49ef48ec22d5b8e2e0
Mario Zamora <maza1387@gmail.com>
2017-05-08 18:12:02 -0700

    Update JS

--- assets/js/skurt.js
+++ assets/js/skurt.js
@@ -2,19 +2,24 @@ $(document).ready(function() {
 
   var phone;
 
-        // $('#submit').on('click', function() {
+        $('#submit').on('click', function() {
 
-          phone = $('#phoneNumber').val().trim();
+            phone = $('#phoneNumber').val();
+            if(phone != "") {
+              $.ajax({
+                method: "POST",
+                url: "https://www.skurt.com/api/sendLink",
+                data: { "linkId": "default", "phone": phone }
+              })
+                .done(function(response) {
+                  console.log(response);
+                  $('#phoneNumber').val("");
+                });
+              } else {
+                alert("enter proper phone number");
+              }
+        });
 
-            $.ajax({
-              method: "POST",
-              url: "https://www.skurt.com/api/sendLink",
-              data: { linkId: "default", phone: phone }
-            })
-              .done(function(response) {
-                console.log(response);
-              });
 
-        // });
 
 });
